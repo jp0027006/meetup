3e88b8d01f2db6543e4dbc130e431c689a240144
BOSSARYAN <aryanmehta2649@gmail.com>
2024-06-13 03:50:36 +0530

    ALL DONE just need to change css now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar/NavBar';
 import Home_Into from './Home_Intro/Home_Intro';
 import Events_Near from './Events_Near/Events_Near';
 import UpcomingOnlineEvents from './UpcomingEvents/UpcomingOnlineEvents';
+import JoinMeetUp_Signup from './JoinMeetUp_Signup/JoinMeetUp_Signup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Home_Into/>
             <Events_Near/>
             <UpcomingOnlineEvents/>
+            <JoinMeetUp_Signup/>
         </div>
     </div>
   );
