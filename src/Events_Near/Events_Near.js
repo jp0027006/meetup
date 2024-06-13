125f21a5343b481fed714e12a8f1efba7fb8dc30
Yash <yashrajmehta3027@gmail.com>
2024-06-13 19:49:27 +0530

    css upgraded

--- src/Events_Near/Events_Near.js
+++ src/Events_Near/Events_Near.js
@@ -8,7 +8,7 @@ function Events_Near() {
     return (
         <div className="Events_Near">
             <div className="event_header">
-                <h2>Events near <button className="btn event_near_btn">Ahmedabad, IN <div className="event_near_btn_img">  </div> </button> </h2>
+                <h2>Events near <button className="btn event_near_btn">Ahmedabad, IN <div className="event_near_btn_img"> <img src="https://secure.meetupstatic.com/next/images/design-system-icons/edit.svg" /> </div> </button> </h2>
                 <a href="#" className="see_all_event_link"> See all events </a>
             </div>
             <div className="event_main_sec">
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import './NavBar.css';
 import search_icon from '../images/search_icon.png';
 import logo from'../images/meetup_logo.png';
+import world_icon from '../images/world.png'
 
 function NavBar() {
     return (
@@ -12,7 +13,14 @@ function NavBar() {
                         <img src={logo} alt="Bootstrap" width="100" height="100"/>
                     </a>
                     <form className="d-flex me-auto form_search" role="search">
-                        <input className="form-control search_sec_1 " type="search" placeholder="Search events" aria-label="Search"/>
+                        <div className="form-control search_sec_1">
+                            <div className="search_sec_1_part1">
+                                <img src="https://secure.meetupstatic.com/next/images/design-system-icons/search-outline.svg" alt="S" />
+                            </div>
+                            <div className="search_sec_1_part2">
+                                 <input className="search_filed" type="search" placeholder="Search events" aria-label="Search"  />
+                            </div>
+                        </div>
                         <input className="form-control search_sec_2" type="search" placeholder="Ahmedabad, IN" aria-label="Search"/>
                         <button className="btn search_btn" type="submit">
                             <img src={search_icon} alt="icon_not_found" height="100%" width="100%"/>
@@ -24,7 +32,13 @@ function NavBar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active me-2" aria-current="page" href="#">English</a>
+                                <button className="btn btn-link launguage_btn me-1"> 
+
+                                    <img src="https://secure.meetupstatic.com/next/images/design-system-icons/globe-outline.svg"/>
+
+                                     English
+                                     
+                                </button>
                             </li>
                             <li className="nav-item">
                                 <button type="button" className="btn btn-link login_btn me-2">Log in</button>
