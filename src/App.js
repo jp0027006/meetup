3eda53fbfb29a2afe4d85843a281a3f93eb9a534
Jay Patel <jp0027006@gmail.com>
2024-06-13 17:21:42 +0530

    Completed till How meetup work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import NavBar from './NavBar/NavBar';
-import Home_Into from './Home_Intro/Home_Intro';
-import Events_Near from './Events_Near/Events_Near';
+import "./App.css";
+import NavBar from "./NavBar/NavBar";
+import Home_Into from "./Home_Intro/Home_Intro";
+import Events_Near from "./Events_Near/Events_Near";
+import Categories from "./Events_categories/Categories";
+import Cities from "./Cities/Cities";
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <div className="parent_component">
-            <Home_Into/>
-            <Events_Near/>
-        </div>
+      <NavBar />
+      <div className="parent_component">
+        <Home_Into />
+        <Events_Near />
+        <Categories />
+        <Cities />
+      </div>
     </div>
   );
 }
