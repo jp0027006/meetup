a529e5e4e70899c69b8583dc61831c0c7b077c1b
Yash <yashrajmehta3027@gmail.com>
2024-06-16 16:51:28 +0530

    navbar added to eventdetails_page

--- src/EventDetail_Page/EventDetail_Page.jsx
+++ src/EventDetail_Page/EventDetail_Page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import AppData from '../AppData.json';
+import NavBar from '../NavBar/NavBar';
 
 const EventDetail_Page = () => {
     const { id } = useParams();
@@ -13,6 +14,7 @@ const EventDetail_Page = () => {
 
     return (
         <div className="EventDetail_Page">
+            <NavBar />
             <h1>{event.UE_Name}</h1>
             <img src={require(`../images/upcoming_event_photos/${event.UE_Photo}`)} alt={event.UE_Name} />
             <p>{event.UE_HostedBy}</p>
