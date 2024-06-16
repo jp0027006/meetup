7ba82fe0ab9af3f22eb2a8b9c915e00ba4a1f015
Yash <yashrajmehta3027@gmail.com>
2024-06-16 16:36:45 +0530

    LInk To Started Working

--- src/EventDetail_Page/EventDetail_Page.jsx
+++ src/EventDetail_Page/EventDetail_Page.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import AppData from '../AppData.json';
 
-const EventDetail_Page = () =>
-{
-    const {id} = useParams();
-    console.log(id);
-    return(
+const EventDetail_Page = () => {
+    const { id } = useParams();
+    const event = AppData.NearEvent.find(event => event.id === id) || 
+                  AppData.UpcomingOnlineEvents.find(event => event.id === id);
+
+    if (!event) {
+        return <div className="EventDetail_Page">Event not found</div>;
+    }
+
+    return (
         <div className="EventDetail_Page">
-            <p>{id}</p>
+            <h1>{event.UE_Name}</h1>
+            <img src={require(`../images/upcoming_event_photos/${event.UE_Photo}`)} alt={event.UE_Name} />
+            <p>{event.UE_HostedBy}</p>
+            <p>{event.UE_Time_Date}</p>
+            <p>{event.UE_Visitor}</p>
+            <p>{event.UE_price}</p>
         </div>
-    )
-}
+    );
+};
 
 export default EventDetail_Page;
