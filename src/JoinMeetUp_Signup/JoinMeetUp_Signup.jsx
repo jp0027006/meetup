0fcb404eeabb58de3d5d1e8dde79f5d6c8b417ec
Jay Patel <jp0027006@gmail.com>
2024-06-17 10:43:03 +0530

    All modal done

--- src/JoinMeetUp_Signup/JoinMeetUp_Signup.jsx
+++ src/JoinMeetUp_Signup/JoinMeetUp_Signup.jsx
@@ -1,22 +1,35 @@
-import React from "react"
-import './JoinMeetUp_Signup.css'
-import join_meetup from '../images/join_meetup_.png'
+import React from "react";
+import "./JoinMeetUp_Signup.css";
+import join_meetup from "../images/join_meetup_.png";
 
-function JoinMeetUp_Signup()
-{
-    return(
-        <div className="JoinMeetUp_Signup">
-            <div className="JoinMeetUp_Signup_part1">
-                <h3>Join Meetup</h3>
-                <p>People use Meetup to meet new people, learn new things, find support, get out of their comfort zones, and pursue their passions, together. Membership is free.</p>
-                <button className="Signup_btn">Sign up</button>
-            </div>
-            <div className="JoinMeetUp_Signup_part2">
-                <img src={join_meetup} alt="join_meetup photo" height="100%" width="100%" />
-            </div>
-            
-        </div>
-    )
+function JoinMeetUp_Signup() {
+  return (
+    <div className="JoinMeetUp_Signup">
+      <div className="JoinMeetUp_Signup_part1">
+        <h3>Join Meetup</h3>
+        <p>
+          People use Meetup to meet new people, learn new things, find support,
+          get out of their comfort zones, and pursue their passions, together.
+          Membership is free.
+        </p>
+        <button
+          className="Signup_btn"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal2"
+        >
+          Sign up
+        </button>
+      </div>
+      <div className="JoinMeetUp_Signup_part2">
+        <img
+          src={join_meetup}
+          alt="join_meetup photo"
+          height="100%"
+          width="100%"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default JoinMeetUp_Signup
+export default JoinMeetUp_Signup;
