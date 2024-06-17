d626f09958f1230399aecb31494ac26d5e9b5a3b
Yash <yashrajmehta3027@gmail.com>
2024-06-17 16:56:58 +0530

    see all events created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home_Page from "./Home_Page/Home_Page";
 import EventDetail_Page from "./EventDetail_Page/EventDetail_Page";
+import See_all_events from "./See_all_events/See_all_events";
+import Events_Near from "./Events_Near/Events_Near";
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home_Page />} />
+          <Route path="/See_all_events" element={<See_all_events />} />
           <Route path="/events-near/:id" element={<EventDetail_Page />} />
         </Routes>
       </BrowserRouter>
---
+++ src/See_all_events/See_all_events.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function See_all_events() {
+  return (
+    <div className="See_all_events">
+      <p>See_all_events</p>
+    </div>
+  );
+}
+
+export default See_all_events;
