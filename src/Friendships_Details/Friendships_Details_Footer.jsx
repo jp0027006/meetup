adc1592f747848d5b0771b0a171bd690d0255066
Yash <yashrajmehta3027@gmail.com>
2024-06-27 22:46:55 +0530

    Friendships_details

--- src/Friendships_Details/Friendships_Details_Footer.jsx
+++ src/Friendships_Details/Friendships_Details_Footer.jsx
@@ -10,23 +10,25 @@ const Friendships_Details_Footer = () => {
         <div className="FDF_footer-content">
           <div className="FDF_footer-section">
             <h3>Do more of what you love</h3>
-            <ul>
-              <li><a href="#" className='footer_part1_sec1'>Find an event</a></li>
-              <li><a href="#" className='footer_part1_sec1'>Start a group</a></li>
-              <li><a href="#" className='footer_part1_sec1'>Get the app</a></li>
+            <ul className='FDF_sec_to_flex'>
+              <li><a href="#" className='FDF_sec1_text' >Find an event</a></li>
+              <div className="standing_line"> | </div>
+              <li><a href="#" className='FDF_sec1_text' > Start a group</a></li>
+              <div className="standing_line"> | </div>
+              <li><a href="#" className='FDF_sec1_text' >Get the app</a></li>
             </ul>
           </div>
-          <div className="FDF_footer-section">
-            <ul>
+          <div className="FDF_footer-section FDF_sec2_sec3">
+            <ul className='FDF_sec2'>
             <li><a href="#">Sign in</a></li>
               <li><a href="#">Log in</a></li>
               <li><a href="#">Help</a></li>
             </ul>
           </div>
-          <div className="FDF_footer-section">
-            <ul>
+          <div className="FDF_footer-section FDF_sec2_sec3">
+            <ul className='FDF_sec2'>
             <li><a href="#">About</a></li>
-              <li><a href="#">Decide & Conquer Book</a></li>
+              <li><a href="#">Decide &<br />Conquer Book</a></li>
               <li><a href="#">Pro</a></li>
             </ul>
           </div>
@@ -39,11 +41,11 @@ const Friendships_Details_Footer = () => {
             </ul>
           </div>
         </div>
-        <div className="FDF_footer-legal">
+        <div className="FDF_footer-legal d-flex">
             <a href="#">Terms of Service</a>  
-              <div className="standing_line">|</div>
+              <div className="standing_line"> | </div>
             <a href="#">Privacy Policy</a> 
-              <div className="standing_line">|</div>
+              <div className="standing_line"> | </div>
             <a href="#">Cookie Policy</a>
         </div>
       </div>
